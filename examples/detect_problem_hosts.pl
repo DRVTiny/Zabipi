ec2c995969b5832720e3b67f57e7d246d1ad4f32
Konovalov Andrey <drvtiny@gmail.com>
2016-01-02 18:12:09 +0300

    Rewrite detect_problem_hosts.pl to bring to conformity with the prologue.pl template

--- examples/detect_problem_hosts.pl
+++ examples/detect_problem_hosts.pl
@@ -6,38 +6,46 @@ use constant {
      TIMEZONE=>'MSK',
 };
 my %SETENV;
-
 BEGIN {
- open (FH,'<',substr($0,0,rindex($0,'/')).'/'.SETENV_FILE) || die 'Cant set environment: '.SETENV_FILE.' not found!';
- %SETENV=map { chomp; $_=~m/^\s*(?<KEY>[A-Za-z0-9_-]+)\s*=\s*(?:(?<QUO>['"])(?<VAL>[^\g{QUO}]+?)\g{QUO}|(?<VAL>[^'"[:space:]]+?))\s*$/?($+{'KEY'},$+{'VAL'}):('NOTHING','NOWHERE') } grep { $_ !~ m/^\s*(?:#.*)?$/ } <FH>;
+ open (my $fhSetEnv,'<',substr($0,0,rindex($0,'/')).'/'.SETENV_FILE) || die 'Cant set environment: '.SETENV_FILE.' not found!';
+ %SETENV=map { chomp; $_=~m/^\s*(?<KEY>[A-Za-z0-9_-]+)\s*=\s*(?:(?<QUO>['"])(?<VAL>[^\g{QUO}]+?)\g{QUO}|(?<VAL>[^'"[:space:]]+?))\s*$/?($+{'KEY'},$+{'VAL'}):('NOTHING','NOWHERE') } grep { $_ !~ m/^\s*(?:#.*)?$/ } <$fhSetEnv>;
  push @INC,split(/\;/,$SETENV{'PERL_LIBS'}) if $SETENV{'PERL_LIBS'};
- close(FH);
+ close($fhSetEnv);
 }
 
-use Monitoring::Zabipi qw(zbx);
+use Monitoring::Zabipi qw(zbx zbx_last_err zbx_api_url);
 no warnings;
 
-my %FE;
-@FE{'login','server','pass'}=@SETENV{'ZBX_LOGIN','ZBX_HOST','ZBX_PASS'};
-
-my $dbg={};
 my $firstarg=shift;
+my $apiPars={'wildcards'=>'true'};
 if ($firstarg eq '-x') {
- $dbg={'debug'=>1};
+ $apiPars->{'debug'}=1;
+ $apiPars->{'pretty'}=1;
 } else {
  unshift @ARGV,$firstarg;
 }
 
-Monitoring::Zabipi->new($FE{'server'},$dbg);
-zbx('auth',$FE{'login'},$FE{'pass'}) || die 'I cant authorize you on '.$FE{'server'}."\!\n";
+die 'You must specify ZBX_HOST or ZBX_URL in your config '.SETENV_FILE 
+ unless my $zbxConnectTo=$SETENV{'ZBX_HOST'} || $SETENV{'ZBX_URL'};
+die 'Cant initialize API, check connecton parameters (ZBX_HOST or ZBX_URL in your config '.SETENV_FILE
+ unless Monitoring::Zabipi->new($zbxConnectTo, $apiPars);
+zbx('auth',@SETENV{'ZBX_LOGIN','ZBX_PASS'}) || 
+ die 'I cant authorize you on ',$zbxConnectTo,". Check your credentials and run this script with the first key '-x' to know why this happens exactly\n";
 
+# Your code goes here ->
 my %Hosts=map {$_->{'hosts'}[0]{'host'}=>1} @{zbx('queue.get')};
+
+do {
+ print STDERR 'No problem hosts found un queue',"\n";
+ exit 0
+} unless %Hosts;
+
 %Hosts=map { $_->{'host'}=>$_->{'interfaces'}[0]{'ip'} } grep {$_}
  ( map { zbx('host.get',{'search'=>{'host'=>$_},  'filter'=>{'status'=>0,'maintenance_status'=>0},               'output'=>['host'],'selectInterfaces'=>['ip']})->[0] } keys %Hosts ),
  (    @{ zbx('host.get',{'search'=>{'error'=>'*'},'filter'=>{'status'=>0,'maintenance_status'=>0,'available'=>2},'output'=>['host'],'selectInterfaces'=>['ip']})      }             );
- 
+
 print join("\n",(map { $_.','.$Hosts{$_} } sort keys %Hosts),'');
 
 END {
- zbx('logout');
+ zbx('logout') if zbx_api_url;
 }
